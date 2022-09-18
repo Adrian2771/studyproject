34ef7df6c1c90f014c574ea52ea91505a4d1ae98
Adrian2771 <adrian_iclozan27@yahoo.ro>
2022-09-18 22:45:34 +0300

    Update substrings.js

--- projectPages/AssignmentBPC_v2/js/substrings.js
+++ projectPages/AssignmentBPC_v2/js/substrings.js
@@ -35,31 +35,32 @@ function Substrings(){
 
 function myFunction(s){
 
-	var temp = s[0];
+	var temp = '';
 
-	var j = 0;
-	while(j <= s.length){
-		var i = j + 1;
-		while(i <= s.length){
-			//console.log(s.substring(j, i));
-			s1 = s.substring(j, i);
-			var k = 0;
-			while(k < s1.length - 1){
-				if(s1.charCodeAt(k) <= s1.charCodeAt(k + 1)){
-					if(temp.length < (s1.substring(0, k + 2)).length){
-						temp = s1.substring(0, k + 2);
-					}
-					k++;
-				}
-				else{
-					break;
-				}
-			}
-			i++;
-		}
-		j++;
-	}
-	return "String: <b>" + s + "</b>. The longest substring in alphabetical order is: <b>" + temp + "</b>";
+	let len = s.length;
+
+  for(let i = 0; i < len; i++){
+    for(let j = i + 1; j <= len; j++){
+      let curr_ss = s.substring(i, j);
+      let longest = true;
+
+      // Check if the current substring is alphabetically ordered
+      for(let k = 0; k < curr_ss.length - 1; k++){
+        if(curr_ss.charCodeAt(k) >= curr_ss.charCodeAt(k + 1)){
+          longest = false;
+          break;
+  		 }
+     }
+		 // If curr_ss is alphabetically ordered and longer than the string stored in the temp variable,
+		 // we'll store curr_ss in temp
+     if(longest){
+       if(temp.length < curr_ss.length){
+         temp = curr_ss;
+       }
+     }
+   }
+ }
+ return "String: <b>" + s + "</b>. The longest substring in alphabetical order is: <b>" + temp + "</b>";
 }
 
 function myFunction1(s){
